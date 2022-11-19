e8561b316837cbb3452d69e2e5c84cf3af731e6d
Woo Ju <universe8897@gmail.com>
2022-11-19 16:47:09 +0900

    bbs앱 model 작성하기, my_page앱 생성하기

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,11 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
--- bbs/admin.py
+++ bbs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Category, Post, Comment
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
+
+admin.site.register(User)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
