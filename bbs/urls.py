34d7c0272e167fac79db06a347f48c2e277cde7f
Woo Ju <universe8897@gmail.com>
2022-11-28 23:15:10 +0900

    게시글 작성페이지 front 구현하기

--- bbs/urls.py
+++ bbs/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('write_post/', views.PostWrite.as_view()),
-    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
 
     # IP주소/bbs/category/slug/
     path('category/<str:slug>/', views.category_list),
--- bbs/views.py
+++ bbs/views.py
@@ -52,24 +52,6 @@ class PostWrite(CreateView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count
         return context
 
-# 게시글 수정 페이지
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ['title', 'category', 'content', 'file']
-    template_name = 'bbs/post_update.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user == self.get_object().author:
-            return super(PostUpdate,self).dispatch(request, *args, **kwargs)
-        else:
-            raise PermissionDenied
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(PostUpdate,self).get_context_data()
-        context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count
-        return context
-
 # 카테고리별 Post 목록 출력 페이지
 def category_list(request, slug):
     if slug == 'no_category':
@@ -81,3 +63,4 @@ def category_list(request, slug):
         'categories' : Category.objects.all(),
         'no_category_post_count' : Post.objects.filter(category=None).count,
     })
+    
